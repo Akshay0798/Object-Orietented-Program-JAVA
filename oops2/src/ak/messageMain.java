4ed9cbcd13c957d47e3284ff158fc18e82b1a053
Akshay Umredkar <akshayumredkar9822@gmail.com>
2024-01-01 23:10:42 +0530

    initial commit

---
+++ oops2/src/ak/messageMain.java
@@ -0,0 +1,10 @@
+package ak;
+
+import static ak.message.akshay;
+
+public class messageMain {
+    public static void main(String[] args) {
+        System.out.println("Suppu is awesome");
+        akshay();
+    }
+}
